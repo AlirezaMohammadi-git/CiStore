e8ecb2d2017d166338cdcd5b146050dc43e5fbad
AlirezaMohammadi <mohammadi09336462@gmail.com>
2023-06-23 22:35:30 +0330

    MASTER : some wrong text fixed.

--- app/src/main/java/com/pixel_alireza/gameland/utils/Constants.kt
+++ app/src/main/java/com/pixel_alireza/gameland/utils/Constants.kt
@@ -5,8 +5,6 @@ package com.pixel_alireza.gameland.utils
 object Constants {
     const val NORMAL_BASE_URL = "http://gate.dunijet2.com:8080"
     const val WS_BASE_URL = "ws://gate.holosen.xyz:8080"
-    const val CLOSE =
-        "r(xGtm7JIG2xf3EB9*IwRn)RTW4*QnHn^pQ)*XKLHXJUX*)P^9kJAccQvY9+X!fcloseSessionp&2G7*JknwGR@Ihurr(xGtm7JIG2xf3EB9*IwRn)RTW4*QnHn^pQ)*XKLHXJUX*)P^9kJAccQvY9+X!fr(xGtm7JIG2xf3EB9*IwRn)RTW4*QnHn^pQ)*XKLHXJUX*)P^9kJAccQvY9+X!f(xGtm7JIG2xf3EB9*IwRn)RTW4*QnHn^pQ)*XKLHXJUX*)P^9kJAccQvY9+X!f"
 }
 
 sealed class ChatState(val state: String) {
